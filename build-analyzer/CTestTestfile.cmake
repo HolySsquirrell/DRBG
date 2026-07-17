85360c8cdbf8170c0eecc8a197fa3a412e3e838f
Marcel <m.jaroszewicz.983@studms.ug.edu.pl>
2026-07-17 13:39:10 +0200

    Code diagnostic

---
+++ build-analyzer/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Marcel/Desktop/Studia/Praktyki/DRBG
+# Build directory: C:/Users/Marcel/Desktop/Studia/Praktyki/DRBG/build-analyzer
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(HashDRBGTests "C:/Users/Marcel/Desktop/Studia/Praktyki/DRBG/build-analyzer/drbg_tests.exe")
+set_tests_properties(HashDRBGTests PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/Marcel/Desktop/Studia/Praktyki/DRBG/CMakeLists.txt;83;add_test;C:/Users/Marcel/Desktop/Studia/Praktyki/DRBG/CMakeLists.txt;0;")
